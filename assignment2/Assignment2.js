e46e43d4c23a4dc91745e9a7c41c2e8506f0c026
karthikkumar nattamai kumaresan <nkkarthik84@gmail.com>
2015-11-15 21:58:30 -0800

    submission
    assignment 2, 3 and 4 submission

---
+++ assignment2/Assignment2.js
@@ -0,0 +1,13 @@
+﻿
+document.getElementById("submit").onclick = display;
+
+function check(validate) {
+    alert(validate(document.getElementById("email").value));
+}
+
+function display() {
+    check(function (email) {
+            var re = /^\S+@\S+\.\S+$/;
+            return re.test(email);
+        });
+}
